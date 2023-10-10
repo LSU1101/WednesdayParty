ccf29c26ec8f0eb37973a18f258cadc920193033
LSU <sangun7573@naver.com>
2023-10-10 13:41:01 +0900

    2/3..?

--- LSU/src/MGPApp.java
+++ LSU/src/MGPApp.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-abstract public class Player {
+abstract class Player {
     protected String[] bet = { "묵", "찌", "빠" };
     protected String name; // 이름
     protected String lastBet = null; // 최근에 낸 값
@@ -15,7 +15,7 @@ abstract public class Player {
 }
 
 class Human extends Player {
-    private Human(String name) {
+    public Human(String name) {
         super(name); // 슈퍼클래스에 접근
     }
 
@@ -23,16 +23,32 @@ class Human extends Player {
     public String next() { // 묵, 찌, 빠 중 입력
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(getName() + ">>");
-
         while (true) {
+            System.out.println(getName() + ">>");
             this.lastBet = scanner.next();
 
             if (this.lastBet.equals("묵") || this.lastBet.equals("찌") || this.lastBet.equals("빠")) {
                 return lastBet;
             }
+            System.out.println("묵, 찌, 빠 중에서 입력하세요.");
+            scanner.next();
         }
+    }
+}
 
+class Computer extends Player {
+    public Computer(String name) {
+        super(name);
+    }
+
+    @Override
+    public String next() {
+        int choose = (int)(Math.random() * 3);
+        this.lastBet = bet[choose];
+
+        System.out.println(getName() + ">> 결정하였습니다.");
+
+        return lastBet;
     }
 }
 
@@ -42,11 +58,12 @@ class Game {
 
     public void run() {
         players = new Player[2];
+        init(players);
 
 
     }
 
-    private Player[] start(Player[] players) {
+    private void init(Player[] players) {
         System.out.println("묵찌빠 게임을 시작합니다.");
 
         System.out.print("선수이름을 입력하세요>> ");
@@ -55,11 +72,15 @@ class Game {
         String computer = scanner.next();
 
         players[0] = new Human(human);
+        players[1] = new Computer(computer);
+
+        System.out.println("두 명의 선수를 생성 완료하였습니다.");
     }
 }
 
 public class MGPApp {
     public static void main(String[] args) {
-
+        Game game = new Game();
+        game.run();
     }
 }
