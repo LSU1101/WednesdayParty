a3153bbe4f7aa4f939cfffd00d561a483aa45b40
LSU <sangun7573@naver.com>
2023-10-10 16:41:33 +0900

    묵찌빠 과제 끝

--- LSU/src/MGPApp.java
+++ LSU/src/MGPApp.java
@@ -24,14 +24,16 @@ class Human extends Player {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println(getName() + ">>");
-            this.lastBet = scanner.next();
+            System.out.print(getName() + ">> ");
+            this.lastBet = scanner.next(); // 입력 받기
 
-            if (this.lastBet.equals("묵") || this.lastBet.equals("찌") || this.lastBet.equals("빠")) {
-                return lastBet;
+            if (this.lastBet.equals("묵") || this.lastBet.equals("찌") || this.lastBet.equals("빠")) { // 사용자가 올바르게 입력했을 경우
+                return lastBet; // 입력한 값 리턴
             }
+
+            // 올바르지 않게 입력했을 경우 실행
             System.out.println("묵, 찌, 빠 중에서 입력하세요.");
-            scanner.next();
+            // scanner.next(); // 입력한 문자 삭제
         }
     }
 }
@@ -43,24 +45,25 @@ class Computer extends Player {
 
     @Override
     public String next() {
-        int choose = (int)(Math.random() * 3);
-        this.lastBet = bet[choose];
+        int choose = (int)(Math.random() * 3); // 0 ,1 ,2 중 랜덤으로 선택
+        this.lastBet = bet[choose]; // 랜덤으로 선택된 choose 값으로 묵, 찌, 빠 중 선택
 
-        System.out.println(getName() + ">> 결정하였습니다.");
+        System.out.println(getName() + ">> 결정하였습니다."); // 결정 완료 메시지
 
-        return lastBet;
+        return lastBet; // 선택된 값 리턴
     }
 }
 
 class Game {
     Scanner scanner = new Scanner(System.in);
-    Player[] players;
+    Player[] players; // 배열 생성
 
     public void run() {
         players = new Player[2];
-        init(players);
-
-
+        init(players); // 선수 생성
+        gamePlay();
+        System.out.println("게임을 종료합니다.");
+        scanner.close();
     }
 
     private void init(Player[] players) {
@@ -68,13 +71,40 @@ class Game {
 
         System.out.print("선수이름을 입력하세요>> ");
         String human = scanner.next();
-        System.out.print("선수이름을 입력하세요>> ");
+        System.out.print("컴퓨터이름을 입력하세요>> ");
         String computer = scanner.next();
 
+        // 입력 받은 이름으로 생성
         players[0] = new Human(human);
         players[1] = new Computer(computer);
 
         System.out.println("두 명의 선수를 생성 완료하였습니다.");
+    }
+
+    private void gamePlay() {
+        int owner = 0; // 0 = human, 1 = computer
+
+        while (true) {
+            // 입력 받기
+            players[owner].next();
+            players[(owner * -1) + 1].next();
+
+            // 무엇을 냈는지 출력
+            for (int i = 0; i < 2; i++) {
+                System.out.print(players[i].getName() + ": " + players[i].getBet() + ", ");
+            }
+            System.out.println("\n");
+
+            if (players[0].getBet().equals(players[1].getBet()) && owner == 0) { // owner가 0일 때 같은 것을 냈을 경우 0이 승리
+                System.out.println(players[0].getName() + "이(가) 이겼습니다!");
+                break;
+            } else if (players[0].getBet().equals(players[1].getBet()) && owner == 1) { // owner가 1일 때 같은 것을 냈을 경우 1이 승리
+                System.out.println(players[1].getName() + "이(가) 이겼습니다!");
+                break;
+            } else if (players[owner].getBet().equals("묵") && players[(owner * -1) + 1].getBet().equals("빠") || players[owner].getBet().equals("찌") && players[(owner * -1) + 1].getBet().equals("묵") || players[owner].getBet().equals("빠") && players[(owner * -1) + 1].getBet().equals("찌")) { // 서로 다른 것을 내고 owner가 가위바위보에서 졌을 경우 오너를 변경
+                owner = owner * -1 + 1;
+            }
+        }
     }
 }
 
